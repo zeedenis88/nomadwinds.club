d7b641f08cb3bd4d511c11424ded7342884e8ecc
Denis <denis.miaillier@gmail.com>
2025-04-19 14:19:56 +0200

    add proper images

--- brazil/scripts/main.js
+++ brazil/scripts/main.js
@@ -1,3 +1,23 @@
+const slideContainer = document.querySelector('.glide__slides');
+const totalImages = 21;
+
+for (let i = 1; i <= totalImages; i++) {
+  const li = document.createElement('li');
+  li.className = 'glide__slide';
+
+  const a = document.createElement('a');
+  a.href = `./images/gallery/${i}.webp`;
+  a.setAttribute('data-fslightbox', '');
+
+  const img = document.createElement('img');
+  img.src = `./images/gallery/${i}.webp`;
+  img.alt = `Image ${i}`;
+
+  a.appendChild(img);
+  li.appendChild(a);
+  slideContainer.appendChild(li);
+}
+
 new Glide('.glide', {
   autoplay: 3000,
   hoverpause: true,
